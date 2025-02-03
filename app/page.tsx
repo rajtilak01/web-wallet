f6c703e4c6832b8d1732588e3d82e9b15b6d2a82
Rajtilak Pandey <rajtilakpandey16@gmail.com>
2025-02-03 21:42:06 +0530

    visible mnemonic

--- app/page.tsx
+++ app/page.tsx
@@ -8,30 +8,46 @@ import { EthWallet } from "@/components/EthWallet";
 
 export default function Home() {
   const [mnemonic, setMnemonic] = useState("");
+  const [showMnemonic, setShowMnemonic] = useState(false);
   const router = useRouter();
 
-  function ethFunction() {
-    router.push("/wallets/eth")
-  }
+  // function ethFunction() {
+  //   router.push("/wallets/eth")
+  // }
 
-  function solanaFunction() {
-    router.push("/wallets/solana")
-  }
+  // function solanaFunction() {
+  //   router.push("/wallets/solana")
+  // }
   
   async function generateMnemonicFunction() {
     const mn = await generateMnemonic();
     setMnemonic(mn)
+    setShowMnemonic(true)
   }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 flex flex-col max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="text-3xl my-6">Choose a wallet to connect?</p>
-        <input type="text" value={mnemonic}></input>
-        <div className="flex gap-4">
-          <Button onClick={ethFunction}>Etherium</Button>
-          <Button onClick={solanaFunction}>Solana</Button>
+        {/* <p className="text-3xl my-6">Choose a wallet to connect?</p> */}
+        <div className="flex gap-4 flex-col">
+          {/* <Button onClick={ethFunction}>Etherium</Button>
+          <Button onClick={solanaFunction}>Solana</Button> */}
+          <div className="justify-center items-center bg-red-500 flex">
+
           <Button onClick={generateMnemonicFunction}>Generate Seed Phrase</Button>
+          </div>
+
+          <div className="flex gap-4">
+          {showMnemonic && mnemonic.split(" ").map((word, index) => (
+              <p
+                key={index}
+                className="md:text-lg bg-foreground/5 hover:bg-foreground/10 transition-all duration-300 rounded-lg p-4"
+              >
+                {word}
+              </p>
+            ))}
+          </div>
+         
         </div>
         <div>
           <SolanaWallet mnemonic={mnemonic}/>
